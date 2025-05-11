e37713799f8ad5c242e4acc828756fb693158868
JosiahFu <josiahfu@gmail.com>
2025-05-11 03:28:11 -0700

    WIP

--- src/client/java/archives/tater/offhandhotbar/OffhandHotbar.java
+++ src/client/java/archives/tater/offhandhotbar/OffhandHotbar.java
@@ -7,53 +7,68 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class OffhandHotbar implements ClientModInitializer {
 	public static int selectedOffhandSlot = 0;
 	private static int lastOffhandSlot = selectedOffhandSlot;
 	public static boolean swapped = false;
 
+	public static final int OFFHAND_SWAP_ID = 40;
+	public static final int SLOTS_OFFSET = 18;
+
 	public static final float HOTBAR_WIDTH = 91;
 	public static final float HOTBAR_GAP = 4;
 	public static final float HOTBAR_OFFSET = HOTBAR_WIDTH + HOTBAR_GAP / 2;
 
 	public static int getOffhandHotbarSlot(int selectedSlot) {
-		return selectedSlot + 27;
+		return PlayerScreenHandler.INVENTORY_START + SLOTS_OFFSET + selectedSlot;
 	}
 
-	public static void cycle(MinecraftClient client, int slot1, int slot2, int slot3) {
+	public static void offhandCycle(MinecraftClient client, int slot1, int slot2) {
 		var interactionManager = client.interactionManager;
 		if (interactionManager == null) return;
 		var player = client.player;
 		if (player == null) return;
-		cycle(interactionManager, player, slot1, slot2, slot3);
+		offhandCycle(interactionManager, player, slot1, slot2);
 	}
 
-	public static void cycle(ClientPlayerInteractionManager interactionManager, ClientPlayerEntity player, int slot1, int slot2, int slot3) {
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot1, 0, SlotActionType.PICKUP, player);
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot2, 0, SlotActionType.PICKUP, player);
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot3, 0, SlotActionType.PICKUP, player);
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot1, 0, SlotActionType.PICKUP, player);
+	public static void offhandCycle(ClientPlayerInteractionManager interactionManager, ClientPlayerEntity player, int slot1, int slot2) {
+		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot1, OFFHAND_SWAP_ID, SlotActionType.SWAP, player);
+		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot2, OFFHAND_SWAP_ID, SlotActionType.SWAP, player);
 	}
 
-	public static void swap(MinecraftClient client, int slot1, int slot2) {
+	public static void swapOffhand(MinecraftClient client, int slot) {
 		var interactionManager = client.interactionManager;
 		if (interactionManager == null) return;
 		var player = client.player;
 		if (player == null) return;
-		swap(interactionManager, player, slot1, slot2);
+		swapOffhand(interactionManager, player, slot);
 	}
 
-	public static void swap(ClientPlayerInteractionManager interactionManager, ClientPlayerEntity player, int slot1, int slot2) {
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot1, 0, SlotActionType.PICKUP, player);
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot2, 0, SlotActionType.PICKUP, player);
-		interactionManager.clickSlot(player.playerScreenHandler.syncId, slot1, 0, SlotActionType.PICKUP, player);
+	public static void swapOffhand(ClientPlayerInteractionManager interactionManager, ClientPlayerEntity player, int slot) {
+		interactionManager.clickSlot(player.currentScreenHandler.syncId, slot, OFFHAND_SWAP_ID, SlotActionType.SWAP, player);
 	}
 
-	public static void onOpenScreen(MinecraftClient client) {
-		swap(client, PlayerScreenHandler.INVENTORY_START + 18 + selectedOffhandSlot, PlayerScreenHandler.OFFHAND_ID);
-		swapped = false;
+	public static @Nullable List<Slot> getScreenSlots(MinecraftClient client) {
+		if (client.currentScreen == null)
+			return null;
+
+		if (client.player == null) return null;
+		if (client.player.currentScreenHandler == null) return null;
+		if (client.player.currentScreenHandler instanceof PlayerScreenHandler) return null;
+		return client.player.currentScreenHandler.slots;
+	}
+
+	public static int findOffhandSlot(List<Slot> slots, int slotIndex, ClientPlayerEntity player) {
+		for (var slot : slots)
+			if (slot.inventory == player.getInventory() && slot.getIndex() == slotIndex)
+				return slot.id;
+		return -1;
 	}
 
 	@Override
@@ -61,19 +76,29 @@ public class OffhandHotbar implements ClientModInitializer {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (selectedOffhandSlot != lastOffhandSlot) {
-				cycle(client,
-						PlayerScreenHandler.INVENTORY_START + 18 + selectedOffhandSlot,
-						PlayerScreenHandler.OFFHAND_ID,
-						PlayerScreenHandler.INVENTORY_START + 18 + lastOffhandSlot);
+				offhandCycle(client,
+						getOffhandHotbarSlot(lastOffhandSlot),
+						getOffhandHotbarSlot(selectedOffhandSlot));
 				lastOffhandSlot = selectedOffhandSlot;
-			} else if (!swapped && client.player != null && (client.player.currentScreenHandler == null || client.player.currentScreenHandler instanceof PlayerScreenHandler)) {
-				// TODO: this gets called before currentScreeHandler is set for some reason??? Threading possibly?
-				swap(client, PlayerScreenHandler.INVENTORY_START + 18 + selectedOffhandSlot, PlayerScreenHandler.OFFHAND_ID);
-				swapped = true;
 			}
-		});
+            if (client.player == null) return;
+
+			var slots = getScreenSlots(client);
+
+            if (slots == null) {
+                if (!swapped) {
+                    swapOffhand(client, getOffhandHotbarSlot(selectedOffhandSlot));
+                    swapped = true;
+                }
+            } else {
+                if (swapped) {
+                    swapOffhand(client, findOffhandSlot(slots, getOffhandHotbarSlot(selectedOffhandSlot), client.player));
+                    swapped = false;
+                }
+            }
+        });
 		ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-			swap(client, PlayerScreenHandler.INVENTORY_START + 18 + selectedOffhandSlot, PlayerScreenHandler.OFFHAND_ID);
+			swapOffhand(client, getOffhandHotbarSlot(selectedOffhandSlot));
 			swapped = false;
 		});
 	}
