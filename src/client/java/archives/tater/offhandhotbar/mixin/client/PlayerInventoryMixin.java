9378e4f24716630e09373b8d0952a066daf87315
JosiahFu <josiahfu@gmail.com>
2025-05-16 02:48:31 -0700

    Fix scroll flickering and add icon

--- src/client/java/archives/tater/offhandhotbar/mixin/client/PlayerInventoryMixin.java
+++ src/client/java/archives/tater/offhandhotbar/mixin/client/PlayerInventoryMixin.java
@@ -5,6 +5,7 @@ import archives.tater.offhandhotbar.OffhandHotbarConfig;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.Hand;
 import org.objectweb.asm.Opcodes;
@@ -18,9 +19,10 @@ public class PlayerInventoryMixin {
             at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I", opcode = Opcodes.PUTFIELD)
     )
     private void scrollOffhand(PlayerInventory instance, int value, Operation<Void> original) {
-        if (OffhandHotbarConfig.scrollControls == Hand.OFF_HAND ^ OffhandHotbar.CONTROL_OPPOSITE_KEY.isPressed())
+        if (OffhandHotbarConfig.scrollControls == Hand.OFF_HAND ^ OffhandHotbar.CONTROL_OPPOSITE_KEY.isPressed()) {
             OffhandHotbar.selectedOffhandSlot = value;
-        else
+            OffhandHotbar.updateOffhandSlots(MinecraftClient.getInstance());
+        } else
             original.call(instance, value);
     }
 
