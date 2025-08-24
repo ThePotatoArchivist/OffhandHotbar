af2d17523aecc3c4666d5b826d7639b9c655d831
JosiahFu <josiahfu@gmail.com>
2025-08-24 15:21:09 -0700

    Fix mixin

--- src/main/java/archives/tater/offhandhotbar/mixin/client/GameMenuScreenMixin.java
+++ src/main/java/archives/tater/offhandhotbar/mixin/client/GameMenuScreenMixin.java
@@ -1,8 +1,8 @@
 package archives.tater.offhandhotbar.mixin.client;
 
 import archives.tater.offhandhotbar.OffhandHotbar;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,16 +10,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameMenuScreen.class)
-public abstract class GameMenuScreenMixin extends Screen {
-    protected GameMenuScreenMixin(Text title) {
-        super(title);
-    }
-
+public class GameMenuScreenMixin {
     @Inject(
             method = "disconnect",
             at = @At("HEAD")
     )
-    private void unswap(CallbackInfo ci) {
+    private static void unswap(MinecraftClient client, Text disconnectReason, CallbackInfo ci) {
         OffhandHotbar.swapOffhand(client);
     }
 }
