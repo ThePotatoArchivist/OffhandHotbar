3e52af633675e545e402d13613f6c7048e53f466
JosiahFu <josiahfu@gmail.com>
2025-09-08 10:16:19 -0700

    Add invert use hand priority option

--- src/main/java/archives/tater/offhandhotbar/mixin/client/MinecraftClientMixin.java
+++ src/main/java/archives/tater/offhandhotbar/mixin/client/MinecraftClientMixin.java
@@ -2,16 +2,21 @@ package archives.tater.offhandhotbar.mixin.client;
 
 import archives.tater.offhandhotbar.OffhandHotbar;
 import archives.tater.offhandhotbar.OffhandHotbarConfig;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.Hand;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
+    @Unique
+    private static final Hand[] HANDS_INVERTED = {Hand.OFF_HAND, Hand.MAIN_HAND};
+
     @WrapOperation(
             method = "handleInputEvents",
             at = @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I")
@@ -35,5 +40,13 @@ public class MinecraftClientMixin {
         OffhandHotbar.updateFocusSwap((MinecraftClient) (Object) this, false);
         original.call(instance, value);
         OffhandHotbar.updateFocusSwap((MinecraftClient) (Object) this, true);
+    }
+
+    @ModifyExpressionValue(
+            method = "doItemUse",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/Hand;values()[Lnet/minecraft/util/Hand;")
+    )
+    private Hand[] invertHandPriority(Hand[] original) {
+        return OffhandHotbarConfig.invertItemUseHandPriority ? HANDS_INVERTED : original;
     }
 }
